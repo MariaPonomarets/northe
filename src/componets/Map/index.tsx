5dea2026a52626e925640975219a76cc0d68273c
mponomarets <mariia.ponomarets@gmail.com>
2022-05-29 20:16:50 +0300

    update map

--- src/componets/Map/index.tsx
+++ src/componets/Map/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import MapView from 'react-native-maps';
 import styles from './styles';
 
-const Map = () => (
-  <MapView
-    style={styles.container}
-    initialRegion={{
+const Map = () => {
+  const initialRegion = useMemo(
+    () => ({
       latitude: 37.78825,
       longitude: -122.4324,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
-    }}
-  />
-);
+    }),
+    [],
+  );
+  return <MapView style={styles.container} initialRegion={initialRegion} />;
+};
 
 export default Map;
